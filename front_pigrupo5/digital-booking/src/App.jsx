6f5992cbf1ad0cea2fb0b82d5f82340d2ca57920
Julieth <juliethruizga@gmail.com>
2022-10-25 19:33:21 -0500

    feat: login y register

--- front_pigrupo5/digital-booking/src/App.jsx
+++ front_pigrupo5/digital-booking/src/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import Home from './pages/Home';
+import Login from './pages/Login_form';
+import Register from './pages/Register';
+
+
+
 import {
   BrowserRouter,
   Route,
@@ -9,11 +14,14 @@ import {
   Navigate,
 } from "react-router-dom";
 
+
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
       </Routes>
     </div>
   )
---
+++ front_pigrupo5/digital-booking/src/pages/Login_form.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+import Login from '../components/Login';
+import Footer from '../components/Footer';
+
+const Login_form = () => {
+  return (
+    <div>
+        <Header/>
+        <Login/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Login_form;
