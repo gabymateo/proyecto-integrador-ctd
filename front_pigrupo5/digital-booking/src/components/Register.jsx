83d06b0ce7854ef816b2f9ba57b459afd2f71c14
diolopez9848 <diomer2@gmail.com>
2022-11-01 08:04:01 -0500

    finalizada la validación de datos del form register

--- front_pigrupo5/digital-booking/src/components/Register.jsx
+++ front_pigrupo5/digital-booking/src/components/Register.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/form.css';
 import { NavLink } from 'react-router-dom';
 import { useState } from "react";
+import { isValid } from 'date-fns';
 
 const emailRegexp = new RegExp(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/);
 
@@ -16,12 +17,14 @@ const initValues={
 const LoginRegister = (props) => {
     const [showPwd, setShowPwd] = useState(false);
     const [formValues, setFormValues] = useState(initValues);
-    const [actBotton, setActBotton] = useState(false);
 
-    const [BadEmail, setBadEmail] = useState(false);
-    const [badPassword, setBadPassword] = useState(false);
-    const [difPassword, setDifPassword] = useState(false);
+    const [badName, setBadName] = useState(undefined);
+    const [badLastName, setBadLastName] = useState(undefined);
+    const [badEmail, setBadEmail] = useState(undefined);
+    const [badPassword, setBadPassword] = useState(undefined);
+    const [difPassword, setDifPassword] = useState(undefined);
     const [validationAll, setValidationAll] = useState(false);
+    const [enviarDatos, setEnviarDatos] = useState(false);
 
     const handleChangeFormValues = (e) => {
       setFormValues({
@@ -30,58 +33,73 @@ const LoginRegister = (props) => {
       })
     }
 
-    //---------------------------------INIT Validaciones ----------------------
     const handleSubmit = (e) => {
       e.preventDefault();
-      //const isValid = ((formValues.name).length>1 && (formValues.lastName).length>1 && ((!BadEmail) && !(BadEmail == undefined)) && ((!badPassword) && !(badPassword == undefined)) && ((!difPassword) && !(difPassword == undefined)))
-      const isValid = ((formValues.name).length>1 && (formValues.lastName).length>1 && !BadEmail && !badPassword && !difPassword)
-      console.log("total validaciones: ",isValid);
-      console.log("BadEmail: ", BadEmail);
-      console.log("Badlong pass: ", badPassword);
-      console.log("Badsame pass: ", difPassword)
-      setValidationAll(isValid)
-      hadleBlurEmail();
-      hadleBlurPass();
-      hadleBlurDifPass(); 
+      setEnviarDatos(true)
+      console.log("ENVIADOS");
+    }
+
+    //---------------------------------INIT Validaciones ----------------------
+
+      // console.log("BadName: ", badName);
+      // console.log("BadLastName: ", badLastName);
+      // console.log("BadEmail: ", badEmail);
+      // console.log("Badlong pass: ", badPassword);
+      // console.log("Badsame pass: ", difPassword)
+    
+      
+      React.useEffect(()=>{
+      //const  handleAll=() =>{
+        const isValid = ((badName==false) && (badLastName==false) && (badEmail==false) && (badPassword==false) && (difPassword==false))
+        setValidationAll(isValid)
+        console.log("total validaciones: ",isValid);
+      //}
+    }, [badName, badLastName, badEmail, badPassword, difPassword])
+
+
+    const hadleBlurName= () =>{
+      const hasError = !((formValues.name).length>1)
+      setBadName(hasError)
+      //handleValidationAll();
+    }
+
+    const hadleBlurLastName= () =>{
+      const hasError = !((formValues.lastName).length>1)
+      setBadLastName(hasError)
+      //handleValidationAll();
     }
 
     const hadleBlurEmail = () =>{
       const hasError = ! emailRegexp.test(formValues.email);
       setBadEmail(hasError)
+      //handleValidationAll();
     }
 
     const hadleBlurPass = () => {
       const hasError = (formValues.password).length <6;
       setBadPassword(hasError);
+      //handleValidationAll()
     }
 
     const hadleBlurDifPass = () => {
       const difPass = !(formValues.password == formValues.confirmPassword);
       setDifPassword(difPass);
+      //handleValidationAll()
     }
-
-    // React.useEffect(()=>{
- 
-    //     const isValid = ((formValues.name).length>1 && (formValues.lastName).length>1 && !BadEmail && !badPassword && !difPassword)
-    //     setActBotton(isValid);
-    //     console.log("activación del botton", isValid);
-      
-    // },[hadleBlurEmail,hadleBlurPass, hadleBlurDifPass])
-
-
    //--------------------------------- FIN Validaciones ----------------------
 
     return(
     <div className="container">
         <h2 className="title">Crear Cuenta</h2>
         <form className="form" onSubmit={handleSubmit} > 
-          <label> Nombre  <input name="name" type='text' value={formValues.name} onChange={handleChangeFormValues} /></label>
-          <label> Apellido  <input name="lastName" type='text' value={formValues.lastName} onChange={handleChangeFormValues} /></label>
+          <label> Nombre  <input name="name" type='text' value={formValues.name} onChange={handleChangeFormValues} onBlur={hadleBlurName}/>
+            <span style={{ visibility: badName ? "visible" :"hidden"}}>Por favor ingrese su nombre</span> <br></br> </label>
+          <label> Apellido  <input name="lastName" type='text' value={formValues.lastName} onChange={handleChangeFormValues} onBlur={hadleBlurLastName}/>
+            <span style={{ visibility: badLastName ? "visible" : "hidden"}}>Por favor ingrese su apellido</span> <br></br> </label>
           <label> Correo electrónico  <input name="email" type='email' value={formValues.email} onChange={handleChangeFormValues} onBlur={hadleBlurEmail}/> 
-            <span style={{ visibility: BadEmail ? "visible" : "hidden"}}>No es un email correcto</span> <br></br> </label>
+            <span style={{ visibility: badEmail ? "visible" : "hidden"}}>No es un email correcto</span> <br></br> </label>
           <label> Contraseña <input name="password" type={showPwd ? "text" : "password"} value={formValues.password} onChange={handleChangeFormValues} onBlur={hadleBlurPass} /> 
             <span style={{ visibility: badPassword ? "visible" : "hidden"}}>El password debe tener al menos 6 caracteres</span> <br></br></label>
-          
           <div className="position-absolute pointer pwd-icon" onClick={() => setShowPwd(!showPwd)}>
             {showPwd ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height={"1rem"}>
               <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
@@ -95,10 +113,10 @@ const LoginRegister = (props) => {
 
           <label> Confirmar contraseña <input name="confirmPassword" type='password' value={formValues.confirmPassword} onChange={handleChangeFormValues} onBlur={hadleBlurDifPass} /> 
             <span style={{ visibility: difPassword ? "visible" : "hidden"}}>El password no coincide</span> <br></br></label>
-          {validationAll==true
+          {enviarDatos==true
             ? <p>Datos enviados</p>
             : <p>Debes diligenciar todos los campos</p>}
-          <button className="submit" type='submit' disabled={actBotton}>Crear cuenta</button>
+          <button className="submit" type='submit' disabled={!validationAll}>Crear cuenta</button>
           <div className='footer_form'>
             <p>¿Ya tienes cuenta? <NavLink to="/login" className="active">Iniciar sesión</NavLink></p>
           </div>
