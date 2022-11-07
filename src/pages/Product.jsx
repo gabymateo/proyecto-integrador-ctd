e3b4d7fec204d738ebeb57208658f0c734564039
nahuelranieri <nnranieri.ok@gmail.com>
2022-11-07 01:38:34 -0300

    feat: page products

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/header/Header'
-import Footer from '../components/Footer';
+import Footer from '../components/footer/Footer';
 import { Alojamiento } from '../components/product/Alojamiento';
 
 
