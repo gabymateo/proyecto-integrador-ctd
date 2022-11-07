a8011a301c6204b594b94f04f46e5453bff112dd
gabymateo <mariagabrielamateo@gmail.com>
2022-11-07 20:44:45 -0300

    Barra de Categorías

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import '../../styles/body.css';
 import ListCards from './ListCards';
 import { BloqueBuscador } from './BloqueBuscador';
+import ListCategoryBlock from './ListCategoryBlock';
+
 const Body = () => {
   return (
     <div className='body'>
       <div className='bodyContainer'>
         <BloqueBuscador/>
+        <ListCategoryBlock/>
         <ListCards/> 
       </div>
     </div>
