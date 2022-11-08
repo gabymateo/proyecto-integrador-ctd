f8721ea172b520d989dba3785304f7db4a9bd932
nahuelranieri <nnranieri.ok@gmail.com>
2022-11-07 21:21:26 -0300

    update

--- src/components/product/Alojamiento.jsx
+++ src/components/product/Alojamiento.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { IoChevronBack } from "react-icons/io5";
 import "./alojamiento.css";
 import { NavLink } from "react-router-dom";
 //Iconos
+import { IoChevronBack } from "react-icons/io5";
 import { FiShare2 } from 'react-icons/fi';
 import { AiTwotoneHeart } from 'react-icons/ai';
+//React Slider
+import Slider from "react-slick";
+import "../../../node_modules/slick-carousel/slick/slick.css"; 
+import "../../../node_modules/slick-carousel/slick/slick-theme.css";
 
 export const Alojamiento = () => {
   const images = [
@@ -29,6 +33,19 @@ export const Alojamiento = () => {
       id: 5,
     },
   ];
+  const settings = {
+    responsive: [
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+    ],
+  };
   return (
     <div className="alojamiento">
       <div className="alojamiento__header">
@@ -56,22 +73,24 @@ export const Alojamiento = () => {
         </div>
       </div>
       <div className="alojamiento__imagenes">
-        <div className='imagenes__botones'>
-          <FiShare2 className='share'/>
-          <AiTwotoneHeart className='like'/>
+        <div className="imagenes__botones">
+          <FiShare2 className="share" />
+          <AiTwotoneHeart className="like" />
         </div>
         <div className="imagenes__container">
           {/*
           <div className="imagenes__imagen-principal">
             <img src='https://i.blogs.es/c68014/casa-3d/840_560.jpeg' alt="" />
           </div>
-            */
-          }
+            */}
           <div className="imagenes__imagen-secundaria">
-            {images.map((image) => (
-                <img src={image.src} className={`imagen${image.id}`} />
-            ))}
-            <p>Ver mas</p>
+              {images.map((image) => (
+                <div className={`imagen${image.id}`}>
+                  <img src={image.src}  />
+                </div>
+              ))}
+              <p>Ver mas</p>
+            
           </div>
         </div>
       </div>
@@ -84,7 +103,7 @@ export const Alojamiento = () => {
       <div className="alojamiento__caracteristicas">
         <div className="caracteristicas__container">
           <h1>¿Que ofrece este lugar?</h1>
-          <div className='caracteristicas__barra'></div>
+          <div className="caracteristicas__barra"></div>
           <div className="caracteristicas__atributos">
             <p>Lorem.</p>
             <p>Dolor.</p>
@@ -94,13 +113,13 @@ export const Alojamiento = () => {
             <p>Eligendi?</p>
             <p>Nam?</p>
           </div>
-
         </div>
       </div>
       <div className="alojamiento__politicas">
         <div className="politicas__container">
           <h1>Qué tenés que saber</h1>
-          <div className='caracteristicas__barra'></div>{/**Cambiar nombre a barra */}
+          <div className="caracteristicas__barra"></div>
+          {/**Cambiar nombre a barra */}
           <div className="normas__container">
             <div className="normas">
               <h1>Normas</h1>
