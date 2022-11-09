00e6fee682e274649b9fc4eece5475cce1c840c3
diomer9848 <diomer2@gmail.com>
2022-11-09 17:15:17 -0500

    cambiando nombre del archivo .war

--- src/main/java/com/backend/grupo5/Grupo5Application.java
+++ src/main/java/com/backend/grupo5/Grupo5Application.java
@@ -3,12 +3,19 @@ package com.backend.grupo5;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+//exclude = {SecurityAutoConfiguration.class}
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Grupo5Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
+
 		SpringApplication.run(Grupo5Application.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Grupo5Application.class);
+	}
 }
