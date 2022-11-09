3d94adf0b7390d942258a816efeb99994aaa4765
cblanco <camilowhite7@gmail.com>
2022-11-08 19:22:28 -0500

    extends servlet

--- src/main/java/com/backend/grupo5/Grupo5Application.java
+++ src/main/java/com/backend/grupo5/Grupo5Application.java
@@ -3,9 +3,10 @@ package com.backend.grupo5;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class Grupo5Application {
+public class Grupo5Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Grupo5Application.class, args);
 	}
