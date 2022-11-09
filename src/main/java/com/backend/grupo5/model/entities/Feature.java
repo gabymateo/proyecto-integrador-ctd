86e675e3695746e37659f0a90415a217f84e6289
cblanco <camilowhite7@gmail.com>
2022-11-08 21:07:11 -0500

    fix image entity

--- src/main/java/com/backend/grupo5/model/entities/Feature.java
+++ src/main/java/com/backend/grupo5/model/entities/Feature.java
@@ -1,4 +1,6 @@
 package com.backend.grupo5.model.entities;
 
+import javax.persistence.Column;
+
 public class Feature {
 }
--- src/main/java/com/backend/grupo5/model/entities/Image.java
+++ src/main/java/com/backend/grupo5/model/entities/Image.java
@@ -19,6 +19,8 @@ public class Image {
     @Column
     private String name_key;
 
+    //test
+
     @Column
     private String imageUrl;
 
