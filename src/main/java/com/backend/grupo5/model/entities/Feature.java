03316c9717743d5969de4288c4038812040ba3d9
cblanco <camilowhite7@gmail.com>
2022-11-08 21:08:19 -0500

    update image entity

--- src/main/java/com/backend/grupo5/model/entities/Feature.java
+++ src/main/java/com/backend/grupo5/model/entities/Feature.java
@@ -1,6 +1,12 @@
 package com.backend.grupo5.model.entities;
 
-import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 public class Feature {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
