054a92c69cbb84c35b69c5f653bfb76e4999cb80
cblanco <camilowhite7@gmail.com>
2022-11-08 20:57:32 -0500

    fix image entity

--- src/main/java/com/backend/grupo5/model/entities/Image.java
+++ src/main/java/com/backend/grupo5/model/entities/Image.java
@@ -1,11 +1,9 @@
 package com.backend.grupo5.model.entities;
 
-
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
-import java.net.URL;
+
 
 @Entity
 @Table(name = "images")
@@ -13,36 +11,26 @@ public class Image {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
     private Long id;
 
     @Column(nullable = false)
     private String bucketName;
 
     @Column
-    private String key;
+    private String name_key;
 
-    private URL url;
+    @Column
+    private String imageUrl;
 
     @ManyToOne
     @JoinColumn(name = "product_id")
     @JsonBackReference
     Product product;
 
-//    @Column(nullable = false)
-//    private String url;
 
     public Long getId() {
         return id;
     }
-
-//    public String getUrl() {
-//        return url;
-//    }
-//
-//    public void setUrl(String url) {
-//        this.url = url;
-//    }
 
     public Product getProduct() {
         return product;
@@ -61,26 +49,25 @@ public class Image {
     }
 
     public String getKey() {
-        return key;
+        return name_key;
     }
 
     public void setKey(String key) {
-        this.key = key;
+        this.name_key = key;
     }
 
     public Image() {
-
     }
     public Image(String bucketName, String key) {
         this.bucketName = bucketName;
-        this.key = key;
+        this.name_key = key;
     }
 
-    public void setUrl(URL url) {
-        this.url = url;
+    public void setUrl(String url) {
+        this.imageUrl = url;
     }
 
-    public URL getUrl() {
-        return url;
+    public String getUrl() {
+        return imageUrl;
     }
 }
