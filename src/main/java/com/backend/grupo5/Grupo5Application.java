170fdf9978ed8dc89c542861f430d6073a6f7e51
cblanco <camilowhite7@gmail.com>
2022-11-22 20:07:43 -0500

    fix multipart

--- src/main/java/com/backend/grupo5/Grupo5Application.java
+++ src/main/java/com/backend/grupo5/Grupo5Application.java
@@ -3,10 +3,14 @@ package com.backend.grupo5;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication()
 public class Grupo5Application extends SpringBootServletInitializer {
@@ -27,5 +31,13 @@ public class Grupo5Application extends SpringBootServletInitializer {
 				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
 			}
 		};
+	}
+
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setMaxFileSize(DataSize.ofBytes(100000000L));
+		factory.setMaxRequestSize(DataSize.ofBytes(100000000L));
+		return factory.createMultipartConfig();
 	}
 }
