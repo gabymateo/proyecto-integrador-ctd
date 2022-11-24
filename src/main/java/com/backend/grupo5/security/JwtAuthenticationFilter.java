ede2b866eb1fedae436fc7a47d69aef7ac727682
cblanco <camilowhite7@gmail.com>
2022-11-23 21:39:00 -0500

    token

--- src/main/java/com/backend/grupo5/security/JwtAuthenticationFilter.java
+++ src/main/java/com/backend/grupo5/security/JwtAuthenticationFilter.java
@@ -39,6 +39,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         String token = TokenManagement.createToken(userDetailsImp.getUsername(), userDetailsImp.getPassword(), userDetailsImp.getRole());
 
         response.addHeader("Authorization", "Bearer " + token);
+        response.addHeader("expires", "Bearer " + token);
         response.getWriter().flush();
 
         super.successfulAuthentication(request, response, chain, authResult);
