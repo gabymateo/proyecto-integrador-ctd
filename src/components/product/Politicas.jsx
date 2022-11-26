eb7c437d63fd8f4f74921cd6e5abcd48f9022338
nahuelranieri <nnranieri.ok@gmail.com>
2022-11-25 22:36:31 -0300

    fix: bloque politicas bug

--- src/components/product/Politicas.jsx
+++ src/components/product/Politicas.jsx
@@ -10,9 +10,11 @@ export const Politicas = (props) => {
   React.useEffect(() => {
     getProducts(id);
   }, []);
-  
-  const features = products?.data?.features;
+  const rules = products?.data?.features.filter(x=> x.type!=='security'&&x.type!=='cancelation');
+  const security = products?.data?.features.filter(x=> x.type!=='rules'&&x.type!=='cancelation');
+  const cancelation = products?.data?.features.filter(x=> x.type!=='rules'&& x.type!== 'security');
 
+ 
   return (
     <>
       <div className="alojamiento__politicas">
@@ -20,7 +22,7 @@ export const Politicas = (props) => {
           <h1>Qué tenés que saber</h1>
           <div className="caracteristicas__barra"></div>
           {/**Cambiar nombre a barra */}
-          <div className="normas__container">
+          {/*<div className="normas__container">
             {features?.map((politica) => {
               return (
                 <div key={politica.id} className="normas">
@@ -28,10 +30,37 @@ export const Politicas = (props) => {
                   <p>{politica.name}</p>
                 </div>
               );
-            })}
+            })}*/}
+            <div className='normas__container'>
+              <div className="normas">
+                <h1>Normas</h1>
+                {rules?.map((rules)=>{
+                  return(
+                    <p>{rules.name}</p>
+                  )
+                })}
+              </div>
+              <div className="seguridad">
+                <h1>Seguridad</h1>
+                {security?.map((rules)=>{
+                  return(
+                    <p>{rules.name}</p>
+                  )
+                })}
+              </div>
+              <div className="cancelacion">
+                <h1>Política de Cancelación</h1>
+                {cancelation?.map((rules)=>{
+                  return(
+                    <p>{rules.name}</p>
+                  )
+                })}
+              </div>
+
+            </div>
           </div>
         </div>
-      </div>
+      
     </>
   );
 };
